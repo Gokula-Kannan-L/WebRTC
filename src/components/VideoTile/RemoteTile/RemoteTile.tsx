caf32830d6713db8008aad77c3a09a3cc1e29beb
Gokula Kannan L <64990358+Gokula-Kannan-L@users.noreply.github.com>
2024-07-04 23:37:18 +0530

    Update RemoteTile.tsx

--- src/components/VideoTile/RemoteTile/RemoteTile.tsx
+++ src/components/VideoTile/RemoteTile/RemoteTile.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 import { Avatar } from "@mui/material";
+import MicIcon from '@mui/icons-material/Mic';
+import MicOffIcon from '@mui/icons-material/MicOff';
 
 type RemoteTileProps = {
     remotestream: MediaStream
@@ -7,24 +9,30 @@ type RemoteTileProps = {
     username: string
     avatar: string
     video: boolean
+    audio: boolean
 }
-const RemoteTile:FunctionComponent<RemoteTileProps> = ({remotestream, index, username, video, avatar}) => {
-    const VideoRef = useRef<any>();
-
+const RemoteTile:FunctionComponent<RemoteTileProps> = ({remotestream, index, username, video, avatar, audio}) => {
+    console.log(video)
+    const VideoRef = useRef<any>(null);
     useEffect( () => {
-        VideoRef.current.srcObject = remotestream;
-    },[remotestream]);
+        if(video && VideoRef.current)
+            VideoRef.current.srcObject = remotestream;
+    },[video]);
 
     return(
         <>
-            {true ?
+            {video ?
             <>
-                <video className={`remote-user-${index}`} ref={VideoRef} autoPlay playsInline  width={'100%'} height={'10%'} style={{objectFit: 'cover', borderRadius: "20px", backgroundColor: 'black'}} controls={false} ></video>
+                <video className={`remote-user-${index}`} ref={VideoRef} autoPlay playsInline  width={'100%'} height={'200px'} style={{objectFit: 'cover', borderRadius: "20px", backgroundColor: 'black'}} controls={false} ></video>
                 <h5 style={{position: 'absolute', bottom: 0, left: '20px', color:'whitesmoke'}}>{username}</h5>
             </>
             :
-            <div style={{width: '100%', height: '250px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: "20px", backgroundColor: 'black'}}>
-                <Avatar sx={{fontSize: '100px', height: '200px', width: '200px', backgroundColor: avatar }} >{username?.[0].toLocaleUpperCase()}</Avatar>
+            <div style={{width: '100%', height: '200px', position: 'relative' ,display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: "20px", backgroundColor: 'black'}}>
+                {audio ? 
+                    <MicIcon sx={{position: 'absolute', top: 0, right: 0, color: 'whitesmoke', padding: '15px'}}/> :
+                    <MicOffIcon sx={{position: 'absolute', top: 0, right: 0, color: 'whitesmoke', padding: '15px'}}/>
+                }
+                <Avatar sx={{fontSize: '3rem', height: '100px', width: '100px', backgroundColor: avatar }} >{username?.[0].toLocaleUpperCase()}</Avatar>
             </div>
             }
         </>

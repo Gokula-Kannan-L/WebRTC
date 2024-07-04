ab23ea6470902b285eba3acfd10c38e191ae23ed
Gokula Kannan L <64990358+Gokula-Kannan-L@users.noreply.github.com>
2024-07-04 17:54:39 +0530

    Update Meeting.tsx

--- src/screens/Meeting/Meeting.tsx
+++ src/screens/Meeting/Meeting.tsx
@@ -1,60 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
+import React from 'react';
 import { Grid } from '@mui/material';
 import LocalTile from '../../components/VideoTile/LocalTile/LocalTile';
 import MeetControls from '../../components/MeetControls/MeetControls';
-import RemoteTile from '../../components/VideoTile/RemoteTile/RemoteTile';
+import RemoteUsers from '../../components/RemoteUsers/RemoteUsers';
 
 const Meeting = () => {
 
-    // const AttendeesCount = useSelector( (state: RootState) => state.meeting.participantsCount);
-    const participants = useSelector( (state: RootState) => state.meeting.participants);
-
     return(
         <Grid height={'100vh'} container display={'flex'} flexDirection={'row'} bgcolor={'#343434'}>
-            {false? 
-                <Grid item xs={12} padding={"20px"} bgcolor={'#343434'} height={'90%'}>
+            <Grid item xs={12} sx={{display: "flex", flexDirection: 'row'}} height={'90%'}>
+                <Grid xs={9} item padding={"20px"} bgcolor={'#343434'}>
                     <LocalTile />
                 </Grid>
-                :
-                <Grid item xs={12} sx={{display: "flex", flexDirection: 'row'}} height={'90%'}>
-                    <Grid xs={9} item padding={"20px"} bgcolor={'#343434'}>
-                        <LocalTile />
-                    </Grid>
-                    <Grid xs={3} item borderRadius={'20px'} bgcolor={'#28282B'} margin={'20px 20px 20px 0px'}>
-                        <div className='remote-container' style={{overflowY: "auto", height: '100%'}}>
-                        {
-                            Object.keys(participants).length > 0 && Object.keys(participants).map( (key, index) => {
-                                const user = participants[key];
-                                
-                                if(user?.IsCurrentUser)
-                                    return;
-                                
-                                console.log(user?.IsCurrentUser, user?.peerConnection);
-                                const peerConnection: RTCPeerConnection = user?.peerConnection;
-                                console.log('new User', index, ':  ', peerConnection)
-                                
-                                if(peerConnection){
-                                    console.log("peerConnection");
-                                    const remoteStream = new MediaStream();
-                                    
-                                    peerConnection.ontrack = (event: RTCTrackEvent) => {
-                                        event.streams[0].getTracks().forEach( (track) => {
-                                            console.log("Track-----",track)
-                                            remoteStream.addTrack(track);
-                                        })
-                                    }
-
-                                    return <div className='remote-tile' style={{padding: '10px', position: 'relative'}} key={index}><RemoteTile remotestream={remoteStream} index={index} username={user.username} video={user.video} avatar={user.avatar} /></div>
-                                }
-                                
-                            })
-                        }
-                        </div>
-                    </Grid>
+                <Grid xs={3} item borderRadius={'20px'} bgcolor={'#28282B'} margin={'20px 20px 20px 0px'}>
+                    <RemoteUsers />
                 </Grid>
-            }
+            </Grid>
+        
             <Grid item xs={12} bgcolor={'#000000'} height={'10%'}>
                 <MeetControls/>
             </Grid>

a06ec8c759d00dc59d5c0555e392822e9492a85e
Gokula Kannan <gokulakannan@TRL-LAPTEMP625.local>
2024-07-05 20:44:01 +0530

    Controls check

--- src/components/VideoTile/RemoteTile/RemoteTile.tsx
+++ src/components/VideoTile/RemoteTile/RemoteTile.tsx
@@ -30,17 +30,17 @@ const RemoteTile:FunctionComponent<RemoteUserType> = ({remoteUser, index}) => {
             {remoteUser.preference.video ?
             <>
                 {remoteUser.preference.audio ? 
-                    <MicIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '25px', filter: "drop-shadow(0px 0px 20px black)"}}/> :
-                    <MicOffIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '25px', filter: "drop-shadow(0px 0px 20px black)"}}/>
+                    <MicIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '25px', filter: "drop-shadow(0px 0px 20px black)", zIndex: 1}}/> :
+                    <MicOffIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '25px', filter: "drop-shadow(0px 0px 20px black)", zIndex: 1}}/>
                 }
-                <video className={`remote-user-${index}`} ref={VideoRef} autoPlay playsInline  width={'100%'} height={'200px'} style={{objectFit: 'cover', borderRadius: "20px", backgroundColor: 'black'}} controls={false} ></video>
+                <video className={`remote-user-${index}`} ref={VideoRef} autoPlay playsInline  width={'100%'} height={'200px'} style={{objectFit: 'cover', borderRadius: "20px", backgroundColor: 'black', transform: "rotateY(180deg)"}} controls={false} ></video>
                 <h5 style={{position: 'absolute', bottom: 0, left: "25px",   color:'whitesmoke'}}>{remoteUser.username}</h5>
             </>
             :
             <div style={{width: '100%', height: '200px', position: 'relative' ,display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: "20px", backgroundColor: 'black'}}>
                 {remoteUser.preference.audio ? 
-                    <MicIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '15px', filter: "drop-shadow(0px 0px 20px black)"}}/> :
-                    <MicOffIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '15px', filter: "drop-shadow(0px 0px 20px black)"}}/>
+                    <MicIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '15px', filter: "drop-shadow(0px 0px 20px black)", zIndex: 1}}/> :
+                    <MicOffIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '15px', filter: "drop-shadow(0px 0px 20px black)", zIndex: 1}}/>
                 }
                 <Avatar sx={{fontSize: '3rem', height: '100px', width: '100px', backgroundColor: remoteUser.avatar }} >{remoteUser.username?.[0].toLocaleUpperCase()}</Avatar>
             </div>

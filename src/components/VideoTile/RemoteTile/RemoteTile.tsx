af2b3dfb51d29cae1967c276ee7f5baf85d857b2
Gokula Kannan <gokulakannan@TRL-LAPTEMP625.local>
2024-07-08 15:48:26 +0530

    Controls check

--- src/components/VideoTile/RemoteTile/RemoteTile.tsx
+++ src/components/VideoTile/RemoteTile/RemoteTile.tsx
@@ -20,11 +20,15 @@ export type RemoteUserType = {
 const RemoteTile:FunctionComponent<RemoteUserType> = ({remoteUser, index}) => {
    
     const VideoRef = useRef<any>(null);
+    const AudioRef = useRef<any>(null);
     useEffect( () => {
         if(VideoRef.current){
             VideoRef.current.srcObject = remoteUser.remoteStream;
-            VideoRef.current.muted = remoteUser?.preference.audio;
         }
+        if(AudioRef.current){
+            AudioRef.current.srcObject = remoteUser.remoteStream;
+        }
+
     },[remoteUser]);
 
     return(
@@ -35,7 +39,8 @@ const RemoteTile:FunctionComponent<RemoteUserType> = ({remoteUser, index}) => {
                     <MicIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '25px', filter: "drop-shadow(0px 0px 20px black)", zIndex: 1}}/> :
                     <MicOffIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '25px', filter: "drop-shadow(0px 0px 20px black)", zIndex: 1}}/>
                 }
-                <video className={`remote-user-${index}`} ref={VideoRef} autoPlay playsInline  width={'100%'} height={'200px'} style={{objectFit: 'cover', borderRadius: "20px", backgroundColor: 'black', transform: "rotateY(180deg)"}} controls={false} ></video>
+                <video className={`remote-user-${index}`} ref={VideoRef} autoPlay playsInline  width={'100%'} height={'200px'} style={{objectFit: 'cover', borderRadius: "20px", backgroundColor: 'black', transform: "rotateY(180deg)"}} controls={false} muted={true} ></video>
+                <audio ref={AudioRef} autoPlay style={{display: 'none'}}></audio>
                 <h5 style={{position: 'absolute', bottom: 0, left: "25px",   color:'whitesmoke'}}>{remoteUser.username}</h5>
             </>
             :
@@ -44,6 +49,7 @@ const RemoteTile:FunctionComponent<RemoteUserType> = ({remoteUser, index}) => {
                     <MicIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '15px', filter: "drop-shadow(0px 0px 20px black)", zIndex: 1}}/> :
                     <MicOffIcon sx={{position: 'absolute', top: 0, right: 0, color: '#F4C430', padding: '15px', filter: "drop-shadow(0px 0px 20px black)", zIndex: 1}}/>
                 }
+                <audio ref={AudioRef} autoPlay style={{display: 'none'}}></audio>
                 <Avatar sx={{fontSize: '3rem', height: '100px', width: '100px', backgroundColor: remoteUser.avatar }} >{remoteUser.username?.[0].toLocaleUpperCase()}</Avatar>
             </div>
             }
